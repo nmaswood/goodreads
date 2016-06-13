24a81730f6a8cc9fd88634ed977e4cc9988c210e
Nasr Maswood <nmaswood@cs.uchicago.edu>
2016-06-12 21:26:21 -0400

    foo

--- process_reviews/process_review.py
+++ process_reviews/process_review.py
@@ -1,5 +1,6 @@
 import csv
 from pymongo import MongoClient
+from collections import defaultdict
 
 class filter_reviews():
 
@@ -9,14 +10,15 @@ class filter_reviews():
         db = client['GOODREADS']
 
         self.incoming_l = db['L_REVIEWS']
-        self.incoming_r = db['R_REVIEWS']
+        self.incoming_r = db['C_REVIEWS']
 
         self.outgoing_l = db['L_REVIEWS_PROCESSED']
-        self.outgoing_r = db['R_REVIEWS_PROCESSED']
+        self.outgoing_r = db['C_REVIEWS_PROCESSED']
 
     def main(self):
 
-        for db_in, db_out in [(self.incoming_l, self.outgoing_l), (self.incoming_r, self.outgoing_r)]:
+        print ("Hello World!")
+        for db_in, db_out in [(self.incoming_r, self.outgoing_r)]:
 
             for item in db_in.find(no_cursor_timeout = True):
 
@@ -30,19 +32,17 @@ class filter_reviews():
                 else:
                     db_out.insert(item)
 
-
 run = filter_reviews()
 run.main()
-
 
 class i_o():
 
     def get_from_csv(self, the_type):
 
         books = {
-        'con': 'Distinctive_CON_Top500_updated.csv',
-        'lib': 'Distinctive_LIB_Top500_updated.csv', 
-        'neither' : 'Distinctive_Neither_Top100_updated.csv'
+        'C': 'Distinctive_CON_Top500_updated.csv',
+        'L': 'Distinctive_LIB_Top500_updated.csv', 
+        'N' : 'Distinctive_Neither_Top100_updated.csv'
         }
 
         with open(books[the_type], 'r') as csv_infile:
@@ -56,6 +56,33 @@ class i_o():
     def get_name(self,the_type):
 
         return self.get_column(self.get_from_csv(the_type),1)[1:]
+
+class process():
+
+    def __init__(self):
+
+        client = MongoClient('localhost', 27017)
+        db = client['GOODREADS']
+
+        self.incoming_l = db['L_REVIEWS_PROCESSED']
+        self.incoming_r = db['R_REVIEWS_PROCESSED']
+
+    def reviews_per_book(self, category):
+
+        db = {'L' : self.incoming_l, 'R' : self.incoming_r}
+
+        for review_item in db.find(no_cursor_timeout = True):
+
+        i_o_instance = i_o()
+
+        books_names  = None
+
+
+
+
+
+
+
 
 
 #run = i_o()
